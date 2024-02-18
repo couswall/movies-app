2d8f8b433228717099f9f87910a77ca0c0d8d5d2
Andre Ignorosa <andreiglu@hotmail.com>
2024-02-18 12:04:20 -0600

    Hero.css - letter spacing added to h1

--- movie-app/src/ui/components/Hero.tsx
+++ movie-app/src/ui/components/Hero.tsx
@@ -26,35 +26,35 @@ export const Hero = () => {
           {
             ( backdropPath )  && 
                 <section 
-                className="hero bg-image text-center" 
-                style={{ 
-                    backgroundImage: `url(https://image.tmdb.org/t/p/original/${ backdropPath })`,
-                    backgroundSize: 'cover',
-                    backgroundPosition: 'top center',
-                    backgroundRepeat: 'no-repeat',
-                    height: '100vh'
-                }}>
+                  className="hero bg-image text-center" 
+                  style={{ 
+                      backgroundImage: `url(https://image.tmdb.org/t/p/original/${ backdropPath })`,
+                      backgroundSize: 'cover',
+                      backgroundPosition: 'center center',
+                      backgroundRepeat: 'no-repeat',
+                      height: '100vh'
+                  }}>
 
 
-                <div 
-                  className="mask h-100 w-100 d-flex flex-column justify-content-center align-items-center" 
-                  style={{ backgroundColor: 'rgba(0, 0, 0, 0.65)'}}>
+                  <div 
+                    className="mask h-100 w-100 d-flex flex-column justify-content-center align-items-center" 
+                    style={{ backgroundColor: 'rgba(0, 0, 0, 0.65)'}}>
 
-                  <div className="p-3">
-                      <h1 className="text-white font-weight-bolder">Welcome</h1>
-                      <p className="text-white text-break fs-5">Millions of movies, TV shows and people to discover. Explore Now</p>
-                      
-                      <form className="mb-4 mt-4 w-60 mx-auto d-flex">
-                          <input    
-                          type="text" 
-                          className="form-control me-2" 
-                          placeholder="Search"
-                          autoComplete= "off"
-                          />
-                          <button className="btn btn-secondary">Search</button>
-                      </form>
+                    <div className="p-3">
+                        <h1 className="text-white font-weight-bolder">Welcome</h1>
+                        <p className="text-white text-break fs-4">Millions of movies, TV shows and people to discover. Explore Now</p>
+                        
+                        <form className="mb-4 mt-4 w-60 mx-auto d-flex">
+                            <input    
+                            type="text" 
+                            className="form-control me-2" 
+                            placeholder="Search"
+                            autoComplete= "off"
+                            />
+                            <button className="btn btn-outline-secondary text-white">Search</button>
+                        </form>
+                    </div>
                   </div>
-                </div>
                 </section>
           }
     </>
