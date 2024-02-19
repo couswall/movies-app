f9dde14c20372c18c03bd20874200e7f14ec72bf
Andre Ignorosa <andreiglu@hotmail.com>
2024-02-18 19:46:45 -0600

    Trending.tsx TimeFilter added

--- movie-app/src/pages/HomePage/components/Trending.tsx
+++ movie-app/src/pages/HomePage/components/Trending.tsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import { useFetch } from "../../../hooks/useFetch";
 import { Carousel } from "../../../ui/components/Carousel";
 
 export const Trending = () => {
 
-    const url: string = `https://api.themoviedb.org/3/trending/all/day`;
+    const [ timeFilter, setTimeFilter ] = useState("day");
+    const url: string = `https://api.themoviedb.org/3/trending/all/${timeFilter}`;
+    
     const { data } = useFetch( url );
     const { results = [] } = !!data && data; 
 
@@ -12,9 +15,18 @@ export const Trending = () => {
         <div className="container">
           <h3 className="text-white">Trending</h3>
 
-          <div className="form-check form-switch">
-            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
-            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Day/Week</label>
+          <div className="form-check form-switch d-flex justify-content-end">
+            <input 
+                className="form-check-input me-2" 
+                type="checkbox" 
+                role="switch" 
+                id="flexSwitchCheckDefault"
+                onChange={() => ( timeFilter === 'day' ) ? setTimeFilter('week') : setTimeFilter('day') }
+            />
+            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">
+                <span className={( timeFilter === 'day') ? 'text-secondary' : ''}>Day</span>/
+                <span className={( timeFilter === 'week') ? 'text-secondary' : ''}>Week</span>
+            </label>
           </div>
         
           <Carousel moviesArray={ results }/>  
