5e400832812b6a440e14723cb75f2805255d69d3
Andre Ignorosa <andreiglu@hotmail.com>
2024-02-19 22:12:00 -0600

    MoviesPage.tsx - useEffect of scroll function added

--- movie-app/src/pages/MoviesPage/MoviesPages.tsx
+++ movie-app/src/pages/MoviesPage/MoviesPages.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useFetch } from "../../hooks/useFetch"
 import { Loading, MovieCard, MovieObject } from "../../ui/components";
 
@@ -10,6 +10,8 @@ export const MoviesPage = () => {
   const url: string = `https://api.themoviedb.org/3/movie/popular`;
   const { data, isLoading } = useFetch( url, `&page=${nextPage}` );
   const { results = [] } = !!data && data; 
+
+  const containerRef = useRef(null);
 
   useEffect(() => {
     setAllMovies(( prevMovies: MovieObject[] ) => {
@@ -23,24 +25,37 @@ export const MoviesPage = () => {
     });
   },[data, nextPage]);
 
-
-
   const loadMoreMovies = () => {
     setNextPage((prevPage) => prevPage + 1);
   };
-  
+
+
+  useEffect(() => {
+    const onscroll = () => {
+      const scrolledTo = window.scrollY + window.innerHeight;
+      const isReachBottom = document.body.scrollHeight - 400 <= scrolledTo;
+      if (isReachBottom) loadMoreMovies();
+    };
+
+    window.addEventListener("scroll", onscroll);
+    return () => {
+      window.removeEventListener("scroll", onscroll);
+    };
+  }, []);
+
+
   return (
-    <section className="movies-section mt-5">
+    <section className="movies-section mt-5" ref= { containerRef }>
       <div className="container">
         <h2 className="mb-3">Explore Movies</h2>
 
-        <div className=" mb-4">
-        <select className="form-select" aria-label="Default select example">
-          <option selected>Select Genres</option>
-          <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option>
-        </select>
+        <div className="mb-4">
+          <select className="form-select bg-dark text-white" aria-label="Default select example">
+            <option defaultValue={""}>Select Genres</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </select>
         </div>
 
         <div className="row">
@@ -51,18 +66,18 @@ export const MoviesPage = () => {
           {
             data && 
               allMovies.map( (movie: MovieObject ) => ( 
-                <div className="col-xxl-2 col-lg-3 col-md-3 col-sm-6 col-6 mb-5" key={ movie.id }>
+                <div className="col-xxl-2 col-lg-3 col-md-3 col-sm-6 col-6 mb-5 animate__animated animate__fadeIn" key={ movie.id }>
                   <MovieCard {...movie}/>
                 </div>
               ))
           }
         </div>
 
-          <button className="btn btn-outline-primary" 
+          {/* <button className="btn btn-outline-primary" 
             onClick={() => loadMoreMovies()}
           >
             Show More
-          </button>
+          </button> */}
       </div>
     </section>
   )
