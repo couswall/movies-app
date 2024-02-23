4075ce6bc874489b5d5cbb12b7d2824751d83c91
Andre Ignorosa <andreiglu@hotmail.com>
2024-02-23 10:05:15 -0600

    Hero.tsx: isLoading condition to load movies

--- movie-app/src/ui/components/Hero.tsx
+++ movie-app/src/ui/components/Hero.tsx
@@ -8,49 +8,48 @@ export const Hero = () => {
   const url: string = `https://api.themoviedb.org/3/movie/now_playing`;
  
   const { data, isLoading } = useFetch( url );
+  const { results } = !!data && data; 
 
   const randomIndex = Math.floor( Math.random() * 20 );
-
-  const backdropPath = data?.results?.[randomIndex]?.backdrop_path;
+  const backdropPath = results?.[randomIndex]?.backdrop_path;
 
   return (
-    <>
-        { isLoading && <Loading/> }
+    <>  
+        {
+          ( isLoading )
+            ? <Loading/>
+            : <section 
+                className="hero bg-image text-center" 
+                style={{ 
+                    backgroundImage: `url(https://image.tmdb.org/t/p/original/${ backdropPath })`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center center',
+                    backgroundRepeat: 'no-repeat',
+                    height: '100vh'
+                }}>
 
-          {
-            ( backdropPath )  && 
-                <section 
-                  className="hero bg-image text-center" 
-                  style={{ 
-                      backgroundImage: `url(https://image.tmdb.org/t/p/original/${ backdropPath })`,
-                      backgroundSize: 'cover',
-                      backgroundPosition: 'center center',
-                      backgroundRepeat: 'no-repeat',
-                      height: '100vh'
-                  }}>
 
+                <div 
+                  className="mask h-100 w-100 d-flex flex-column justify-content-center align-items-center" 
+                  style={{ backgroundColor: 'rgba(0, 0, 0, 0.65)'}}>
 
-                  <div 
-                    className="mask h-100 w-100 d-flex flex-column justify-content-center align-items-center" 
-                    style={{ backgroundColor: 'rgba(0, 0, 0, 0.65)'}}>
-
-                    <div className="p-3">
-                        <h1 className="text-white font-weight-bolder">Welcome</h1>
-                        <p className="text-white text-break fs-4">Millions of movies, TV shows and people to discover. Explore Now</p>
-                        
-                        <form className="mb-4 mt-4 w-60 mx-auto d-flex">
-                            <input    
-                            type="text" 
-                            className="form-control me-2" 
-                            placeholder="Search"
-                            autoComplete= "off"
-                            />
-                            <button className="btn btn-outline-secondary text-white">Search</button>
-                        </form>
-                    </div>
+                  <div className="p-3">
+                      <h1 className="text-white font-weight-bolder">Welcome</h1>
+                      <p className="text-white text-break fs-4">Millions of movies, TV shows and people to discover. Explore Now</p>
+                      
+                      <form className="mb-4 mt-4 w-60 mx-auto d-flex">
+                          <input    
+                          type="text" 
+                          className="form-control me-2" 
+                          placeholder="Search"
+                          autoComplete= "off"
+                          />
+                          <button className="btn btn-outline-secondary text-white">Search</button>
+                      </form>
                   </div>
-                </section>
-          }
+                </div>
+              </section>
+        }
     </>
   )
 }
