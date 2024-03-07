5235a652c79edf08d5ee2eb53857de0a81a40d2c
Andre Ignorosa <andreiglu@hotmail.com>
2024-03-07 11:01:26 -0600

    MovieCard.tsx: Link added to card

--- movie-app/src/ui/components/MovieCard.tsx
+++ movie-app/src/ui/components/MovieCard.tsx
@@ -1,35 +1,40 @@
-
-import { MovieObject } from '.';
+import { Link } from 'react-router-dom';
+import { Result } from '../../interfaces';
 import './styles';
 
-export const MovieCard: React.FC<MovieObject> = ({ title, name, poster_path, vote_average, release_date, first_air_date }) => {
+interface MediaTypeProps extends Result{
+  media_type_props?: string; 
+}
+
+export const MovieCard: React.FC<Result & MediaTypeProps > = ({ id,title, name, poster_path, vote_average, release_date, first_air_date, media_type, media_type_props}) => {
 
 
   const onFormatDate = ( movieDate: any ) => {
-
     const date = new Date( movieDate );
     const options: Intl.DateTimeFormatOptions = { 
       year: 'numeric', 
       month: 'short', 
       day: 'numeric' 
     }; 
-    
     return date.toLocaleDateString('en-US', options);
   }
 
+
   return (
-    <div className="card-movie animate__animated animate__fadeIn" >
-      <div className="card-img-container position-relative mb-3">
-        <img src={`https://image.tmdb.org/t/p/original/${ poster_path}`} alt={ title || name} loading='lazy'/>  
-        <div className="raiting-circle position-absolute">
-          <span className='fw-bold text-black'>{ (vote_average).toFixed(1) }</span>
+    <Link to={ `${ ( media_type === 'tv' || media_type_props === 'tv' ) ? 'tv-serie' : 'movie'}/${id}`}>
+      <div className="card-movie animate__animated animate__fadeIn">
+        <div className="card-img-container position-relative mb-3">
+          <img src={`https://image.tmdb.org/t/p/original/${ poster_path}`} alt={ title || name} loading='lazy'/>  
+          <div className="raiting-circle position-absolute">
+            <span className='fw-bold text-black'>{ (vote_average).toFixed(1) }</span>
+          </div>
+        </div>
+
+        <div className="card-movie-info">
+          <p className='movie-title text-white d-inline-block text-truncate text-wrap w-100'>{ title || name}</p>
+          <p className='text-white-50'>{ onFormatDate( release_date || first_air_date ) }</p>
         </div>
       </div>
-
-      <div className="card-movie-info">
-        <p className='movie-title text-white d-inline-block text-truncate text-wrap w-100'>{ title || name}</p>
-        <p className='text-white-50'>{ onFormatDate( release_date || first_air_date ) }</p>
-      </div>
-    </div>
+    </Link>
   )
 }
