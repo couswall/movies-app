bfff77630640f2bc87868ee282998afab473165e
Andre Ignorosa <andreiglu@hotmail.com>
2024-03-09 10:29:37 -0600

    MovieView.tsx: Details.tsx added

--- movie-app/src/pages/Views/MovieView.tsx
+++ movie-app/src/pages/Views/MovieView.tsx
@@ -1,6 +1,17 @@
+import { useParams } from "react-router-dom"
+import { getMediaById } from "../../helpers";
+import { Details } from "./components";
+
 
 export const MovieView = () => {
+
+  const { movieId = '' } = useParams() as { movieId: string}; 
+
+  const { data, isLoading } = getMediaById('movie', movieId ); 
+  
   return (
-    <h1>MovieView</h1>
+
+    <Details data = { data } isLoading={ isLoading } movieId={ movieId }/>
+    
   )
 }
