9992abc210b5ebe866b01529a5bd1a05241252a2
Andre Ignorosa <andreiglu@hotmail.com>
2024-03-11 23:27:26 -0600

    CastCarousel.tsx: div with row class and title added

--- movie-app/src/pages/Views/components/CastCarousel.tsx
+++ movie-app/src/pages/Views/components/CastCarousel.tsx
@@ -24,56 +24,61 @@ export const CastCarousel: React.FC<CastCarouselProps> = ({movieId}) => {
         ( isLoading ) 
           ? <CastSkeleton/>
           :
-          <div className="carousel-wrapper mt-4">
-            <Swiper
-              loop= { true }
-              slidesPerView={3}
-              spaceBetween={10}
-              autoplay= {{
-                delay: 5000, 
-                disableOnInteraction: false
-              }}
-              modules={[Autoplay]}
-              breakpoints={{
-                575: {
-                  slidesPerView: 3
-                }, 
-                768: {
-                  slidesPerView: 4
-                },
-                991: {
-                  slidesPerView: 7
-                },
-                1200: {
-                  slidesPerView: 9
-                }
-              }}              
-            >
-            {
-              castArray.map( (person: CastElement) => {
+          <div className="row">
+            <h3>Cast</h3>            
+            <div className="carousel-wrapper mt-4">
+              <Swiper
+                loop= { true }
+                slidesPerView={3}
+                spaceBetween={10}
+                autoplay= {{
+                  delay: 5000, 
+                  disableOnInteraction: false
+                }}
+                modules={[Autoplay]}
+                breakpoints={{
+                  575: {
+                    slidesPerView: 3
+                  }, 
+                  768: {
+                    slidesPerView: 4
+                  },
+                  991: {
+                    slidesPerView: 7
+                  },
+                  1200: {
+                    slidesPerView: 9
+                  }
+                }}              
+              >
+                    {
+                      castArray.map( (person: CastElement) => {
 
-                const profilePhoto = person.profile_path ? 'https://image.tmdb.org/t/p/w185/' + person.profile_path : NoPhoto;
+                        const profilePhoto = person.profile_path ? 'https://image.tmdb.org/t/p/w185/' + person.profile_path : NoPhoto;
 
-                return (
-                  <SwiperSlide>
-                    <div className="cast-container" key={person.id}>
-                    <div className="img-cast-container overflow-hidden mx-auto mb-2" style={{ width: '100px', height: '100px', borderRadius:'50%'}}>
-                      <img 
-                        src={ profilePhoto }
-                        loading='lazy'
-                        className='w-100 h-100'
-                        style={{objectFit:'cover'}}
-                      />
-                    </div>
-                    <p className='text-white text-center mb-0' style={{fontSize:'.875rem'}}>{ person.name }</p>
-                    <p className='text-white text-center fw-lighter' style={{fontSize:'.75rem'}}>{ person.character }</p>
-                  </div>
-                  </SwiperSlide>
-                )
-              })
-            }
-        </Swiper>
-      </div>
+                        return (
+                          <SwiperSlide key={person.id} >
+                            <div className="cast-container">
+                            <div className="img-cast-container overflow-hidden mx-auto mb-2" style={{ width: '100px', height: '100px', borderRadius:'50%'}}>
+                              <img 
+                                src={ profilePhoto }
+                                loading='lazy'
+                                className='w-100 h-100'
+                                style={{objectFit:'cover'}}
+                              />
+                            </div>
+                            <p className='text-white text-center mb-0' style={{fontSize:'.875rem'}}>{ person.name }</p>
+                            <p className='text-white text-center fw-lighter' style={{fontSize:'.75rem'}}>{ person.character }</p>
+                          </div>
+                          </SwiperSlide>
+                        )
+                      })
+                    }
+                </Swiper>
+            </div>
+          </div>
+
+          
       }
     </>
   )
