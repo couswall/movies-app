a77177b815ba32622d07c1c948cfc3a16230d7d8
Andre Ignorosa <andreiglu@hotmail.com>
2024-03-11 20:28:34 -0600

    CastCarousel.tsx: CastSkeleton.tsx added

--- movie-app/src/pages/Views/components/CastCarousel.tsx
+++ movie-app/src/pages/Views/components/CastCarousel.tsx
@@ -5,6 +5,7 @@ import { useFetch } from '../../../hooks/useFetch';
 
 import NoPhoto from '../../../../public/assets/avatar.png'; 
 import { CastElement } from '../../../interfaces';
+import { CastSkeleton } from '../skeletons';
 
 interface CastCarouselProps {
   movieId: string; 
@@ -19,56 +20,61 @@ export const CastCarousel: React.FC<CastCarouselProps> = ({movieId}) => {
   
   return (
     <>
-      <div className="carousel-wrapper mt-4">
-        <Swiper
-          loop= { true }
-          slidesPerView={3}
-          spaceBetween={10}
-          autoplay= {{
-            delay: 5000, 
-            disableOnInteraction: false
-          }}
-          modules={[Autoplay]}
-          breakpoints={{
-            575: {
-              slidesPerView: 3
-            }, 
-            768: {
-              slidesPerView: 4
-            },
-            991: {
-              slidesPerView: 7
-            },
-            1200: {
-              slidesPerView: 9
-            }
-          }}              
-        >
-          {
-            castArray.map( (person: CastElement) => {
+      {
+        ( isLoading ) 
+          ? <CastSkeleton/>
+          :
+          <div className="carousel-wrapper mt-4">
+            <Swiper
+              loop= { true }
+              slidesPerView={3}
+              spaceBetween={10}
+              autoplay= {{
+                delay: 5000, 
+                disableOnInteraction: false
+              }}
+              modules={[Autoplay]}
+              breakpoints={{
+                575: {
+                  slidesPerView: 3
+                }, 
+                768: {
+                  slidesPerView: 4
+                },
+                991: {
+                  slidesPerView: 7
+                },
+                1200: {
+                  slidesPerView: 9
+                }
+              }}              
+            >
+            {
+              castArray.map( (person: CastElement) => {
 
-              const profilePhoto = person.profile_path ? 'https://image.tmdb.org/t/p/w185/' + person.profile_path : NoPhoto;
+                const profilePhoto = person.profile_path ? 'https://image.tmdb.org/t/p/w185/' + person.profile_path : NoPhoto;
 
-              return (
-                <SwiperSlide>
-                  <div className="cast-container" key={person.id}>
-                  <div className="img-cast-container overflow-hidden mx-auto mb-2" style={{ width: '100px', height: '100px', borderRadius:'50%'}}>
-                    <img 
-                      src={ profilePhoto }
-                      loading='lazy'
-                      className='w-100 h-100'
-                      style={{objectFit:'cover'}}
-                    />
+                return (
+                  <SwiperSlide>
+                    <div className="cast-container" key={person.id}>
+                    <div className="img-cast-container overflow-hidden mx-auto mb-2" style={{ width: '100px', height: '100px', borderRadius:'50%'}}>
+                      <img 
+                        src={ profilePhoto }
+                        loading='lazy'
+                        className='w-100 h-100'
+                        style={{objectFit:'cover'}}
+                      />
+                    </div>
+                    <p className='text-white text-center mb-0' style={{fontSize:'.875rem'}}>{ person.name }</p>
+                    <p className='text-white text-center fw-lighter' style={{fontSize:'.75rem'}}>{ person.character }</p>
                   </div>
-                  <p className='text-white text-center mb-0' style={{fontSize:'.875rem'}}>{ person.name }</p>
-                  <p className='text-white text-center fw-lighter' style={{fontSize:'.75rem'}}>{ person.character }</p>
-                </div>
-                </SwiperSlide>
-              )
-            })
-          }
+                  </SwiperSlide>
+                )
+              })
+            }
         </Swiper>
       </div>
+      }
     </>
   )
 }
