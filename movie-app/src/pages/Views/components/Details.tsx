4bcfaab78417439ed7c6ed7951f1ce41e6681904
Andre Ignorosa <andreiglu@hotmail.com>
2024-03-11 23:28:14 -0600

    Details.tsx: DetailsSkeleton added with conditional

--- movie-app/src/pages/Views/components/Details.tsx
+++ movie-app/src/pages/Views/components/Details.tsx
@@ -1,11 +1,11 @@
 import { LazyLoadImage } from "react-lazy-load-image-component";
-import { Loading } from "../../../ui/components";
 import { Genre, MovieDetails } from "../../../interfaces/MovieDetails";
 import { useMemo } from "react";
 import { CiPlay1 } from "react-icons/ci";
 
 import '../styles/Details.css'; 
 import { CastCarousel } from ".";
+import { DetailsSkeleton } from "../skeletons";
 
 interface MediaDetailsProps {
     data: MovieDetails; 
@@ -53,88 +53,92 @@ export const Details: React.FC<MediaDetailsProps> = ({ data, isLoading, movieId 
 
 
   return (
-    <section className="container-fluid p-0 position-relative d-flex justify-content-center align-items-center" 
+    <>
+      {
+      ( isLoading ) 
+        ? <DetailsSkeleton/>
+        :
+        <section className="container-fluid p-0 position-relative d-flex justify-content-center align-items-center" 
           style={{ 
-            filter: !isLoading ? "none" : "blur(10px)",
-            transition: "filter 0.6s",
+            // filter: !isLoading ? "none" : "blur(10px)",
+            // transition: "filter 0.6s",
             height: '90vh'
           }}
-    >
+        >
+          
               <LazyLoadImage 
-                src={ `https://image.tmdb.org/t/p/original/${ backdrop_path }` } 
+                src={ `https://image.tmdb.org/t/p/original/${ backdrop_path }` }
                 effect="blur" 
                 width='100%' 
                 height='100%'
+                className="animate__animated animate__fadeIn animate__faster"
                 style={{
                     objectFit: 'cover', 
                     objectPosition: `center center`,
                 }}
+                alt={ title || original_title }
                 />
       
-      <div className="details-section container position-absolute w-100 h-100 mt-5">
-          {
-            isLoading ? <Loading/>
-            :
-            <div className="details-wrapper row position-relative gap-4 mb-5">
-              <div className="img-container-view col-xs-12 col-md-5 col-lg-4 col-xl-4 p-0 mx-auto overflow-hidden">
-                <img 
-                  className="img-movie-view"
-                  src={`https://image.tmdb.org/t/p/original/${ poster_path }`} 
-                  alt={ title || original_title} 
-                  loading="lazy"
-                />
+              <div className="details-section container position-absolute w-100 h-100 mt-5">
+                    <div className="details-wrapper row position-relative gap-4 mb-5 animate__animated animate__fadeIn">
+                      <div className="img-container-view col-xs-12 col-md-5 col-lg-4 col-xl-3 p-0 mx-auto overflow-hidden">
+                        <img 
+                          className="img-movie-view"
+                          src={`https://image.tmdb.org/t/p/original/${ poster_path }`} 
+                          alt={ title || original_title} 
+                          loading="lazy"
+                        />
+                      </div>
+
+                      <div className="col-xs-12 col-md-6 col-lg-7 col-xl-8">
+                        <h3>{ title || original_title } {`(${realseYear})`}</h3>
+                        <p className="fw-lighter fst-italic">{tagline}</p>
+
+                        <div className="d-flex gap-2 mb-3">
+
+                          {
+                            genres.map( ( genre: Genre ) => (
+                              <p className="badge bg-secondary" style={{ fontSize: '13px'}} key={ genre.name }>{genre.name}</p>
+                            ))
+                          }                  
+                        </div>
+
+                        <div className="d-flex gap-2 mb-3 details-buttons">
+                          <div className= "circle-details p-3 d-flex justify-content-center align-items-center">
+                            <span className='text-white fs-3'>{ (vote_average).toFixed(1) }</span>
+                          </div>
+                          <div className="d-flex justify-content-center align-items-center gap-3 button-trailer-container text-white">
+                            <div className= "p-3 play-trailer-button circle-details d-flex justify-content-center align-items-center">
+                              <CiPlay1 className="icon"/>
+                            </div>
+                            <span>Watch trailer</span>
+                          </div>
+                        
+                        </div>
+
+                        <div>
+                          <h5>Overview</h5>
+                          <p className="overview">{ overview }</p>
+
+                          <div className="d-flex gap-3">
+                              <p>Status: <span className="fw-lighter">{ status }</span></p>
+                              <p>Release Date: <span className="fw-lighter">{ relaseDate }</span></p>
+                              <p>Runtime: <span className="fw-lighter">{ runTimeToHours } </span></p>
+                          </div>
+                        </div>
+                        <hr />
+                        
+                        <p>Director: <span className="fw-lighter">Realsed</span></p>
+                        <hr />  
+                      </div>
+                    </div>    
+                      <CastCarousel movieId = { movieId }/>
               </div>
-
-              <div className="col-xs-12 col-md-6 col-lg-7 col-xl-7">
-                <h3>{ title || original_title } {`(${realseYear})`}</h3>
-                <p className="fw-lighter fst-italic">{tagline}</p>
-
-                <div className="d-flex gap-2 mb-3">
-
-                  {
-                    genres.map( ( genre: Genre ) => (
-                      <p className="badge bg-secondary" style={{ fontSize: '13px'}} key={ genre.name }>{genre.name}</p>
-                    ))
-                  }                  
-                </div>
-
-                <div className="d-flex gap-2 mb-3 details-buttons">
-                  <div className= "circle-details p-3 d-flex justify-content-center align-items-center">
-                    <span className='text-white fs-3'>{ (vote_average).toFixed(1) }</span>
-                  </div>
-                  <div className="d-flex justify-content-center align-items-center gap-3 button-trailer-container text-white">
-                    <div className= "p-3 play-trailer-button circle-details d-flex justify-content-center align-items-center">
-                      <CiPlay1 className="icon"/>
-                    </div>
-                    <span>Watch trailer</span>
-                  </div>
-                
-                </div>
-
-                <div>
-                  <h5>Overview</h5>
-                  <p className="overview">{ overview }</p>
-
-                  <div className="d-flex gap-3">
-                      <p>Status: <span className="fw-lighter">{ status }</span></p>
-                      <p>Release Date: <span className="fw-lighter">{ relaseDate }</span></p>
-                      <p>Runtime: <span className="fw-lighter">{ runTimeToHours } </span></p>
-                  </div>
-                </div>
-                <hr />
-                
-                <p>Director: <span className="fw-lighter">Realsed</span></p>
-                <hr />  
-              </div>
-            </div>    
-          }
-
-          <div className="row">
-            <h3>Cast</h3>            
-              <CastCarousel movieId = { movieId }/>
-          </div>
-      </div>
-      <div className="overlay-view col-12 position-absolute w-100 h-100"> </div>
-    </section>
+              <div className="overlay-view col-12 position-absolute w-100 h-100 animate__animated animate__fadeIn animate__faster"> </div>
+            </section>
+    }
+    
+    </>
+    
   )
 }
