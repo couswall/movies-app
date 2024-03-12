f7110d4771d4532fb7660ec47b4bb99a214f515c
Andre Ignorosa <andreiglu@hotmail.com>
2024-03-11 20:23:19 -0600

    CastCarousel.tsx: CastElement from CastInterface added

--- movie-app/src/pages/Views/components/CastCarousel.tsx
+++ movie-app/src/pages/Views/components/CastCarousel.tsx
@@ -1,13 +1,28 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import { Autoplay } from 'swiper/modules';
+import { useFetch } from '../../../hooks/useFetch';
 
-export const CastCarousel = () => {
+import NoPhoto from '../../../../public/assets/avatar.png'; 
+
+
+export const CastCarousel = ({movieId}) => {
+  const { data, isLoading } = useFetch(`https://api.themoviedb.org/3/movie/${movieId}/credits`); 
+
+  const { cast = [] } = !!data && data; 
+  
+  const castArray = cast.filter( person => person.known_for_department === 'Acting'); 
+
+  console.log( castArray )
+  
+  console.log( castArray )
+  
   return (
     <>
       <div className="carousel-wrapper mt-4">
         <Swiper
           loop= { true }
+          slidesPerView={3}
           spaceBetween={10}
           autoplay= {{
             delay: 5000, 
@@ -30,17 +45,27 @@ export const CastCarousel = () => {
           }}              
         >
           {
-            Array(10).fill("").map( (arr, index) => (
-              <SwiperSlide>
-                <div className="cast-container w-100" key={index}>
-                <div className="img-cast-container overflow-hidden mx-auto mb-2" style={{ width: '100px', height: '100px', borderRadius:'50%'}}>
-                  <div className="img-cast bg-primary w-100 h-100"></div>
+            castArray.map( (person, index) => {
+
+              const profilePhoto = person.profile_path ? 'https://image.tmdb.org/t/p/w185/' + person.profile_path : NoPhoto;
+
+              return (
+                <SwiperSlide>
+                  <div className="cast-container" key={person.id}>
+                  <div className="img-cast-container overflow-hidden mx-auto mb-2" style={{ width: '100px', height: '100px', borderRadius:'50%'}}>
+                    <img 
+                      src={ profilePhoto }
+                      loading='lazy'
+                      className='w-100 h-100'
+                      style={{objectFit:'cover'}}
+                    />
+                  </div>
+                  <p className='text-white text-center mb-0' style={{fontSize:'.875rem'}}>{ person.name }</p>
+                  <p className='text-white text-center fw-lighter' style={{fontSize:'.75rem'}}>{ person.character }</p>
                 </div>
-                <p className='text-white text-center mb-0'>Jason Momoa</p>
-                <p className='text-white text-center fw-lighter' style={{fontSize:'14px'}}>Someone cool</p>
-              </div>
-              </SwiperSlide>
-            ))
+                </SwiperSlide>
+              )
+            })
           }
         </Swiper>
       </div>
