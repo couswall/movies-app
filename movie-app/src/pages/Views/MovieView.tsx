cc142ffd09fbdbbf800faee2edfe8337d42b2673
Andre Ignorosa <andreiglu@hotmail.com>
2024-03-14 22:42:35 -0600

    MovieView.tsx: useFetch to get videoData added

--- movie-app/src/pages/Views/MovieView.tsx
+++ movie-app/src/pages/Views/MovieView.tsx
@@ -1,18 +1,19 @@
 import { ScrollRestoration, useParams } from "react-router-dom"
 import { getMediaById } from "../../helpers";
 import { Details } from "./components";
+import { useFetch } from "../../hooks/useFetch";
 
 
 export const MovieView = () => {
 
   const { movieId = '' } = useParams() as { movieId: string}; 
-
   const { data, isLoading } = getMediaById('movie', movieId ); 
+  const { data: videoData } = useFetch(`https://api.themoviedb.org/3/movie/${movieId}/videos`)
   
   return (
 
     <>
-      <Details data = { data } isLoading={ isLoading } movieId={ movieId } mediaTypeApi='movie'/>
+      <Details data = { data } isLoading={ isLoading } movieId={ movieId } mediaTypeApi='movie' videoData = { videoData }/>
     
       
       <ScrollRestoration/>
