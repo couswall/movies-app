9c3e1360fb9a7a35297f8dff63c331943874f123
Andre Ignorosa <andreiglu@hotmail.com>
2024-03-20 14:28:47 -0600

    VideoSectionSkeleton.tsx: Classes and structure definded

--- movie-app/src/pages/Views/skeletons/VideoSectionSkeleton.tsx
+++ movie-app/src/pages/Views/skeletons/VideoSectionSkeleton.tsx
@@ -1,6 +1,10 @@
 
+import '../styles/VideoSection.css'
+
 export const VideoSectionSkeleton = () => {
   return (
-    <div>VideoSectionSkeleton</div>
+    <div className="video-container">
+      <div className="w-100 h-100 skeleton"></div>
+    </div>
   )
 }
