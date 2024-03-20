e20d7947c428c6d337df1d67700bedc39896d659
Andre Ignorosa <andreiglu@hotmail.com>
2024-03-20 13:52:40 -0600

    MovieView.tsx: showPopUpVideo State created and added to Details component props and Video Section Props

--- movie-app/src/pages/Views/MovieView.tsx
+++ movie-app/src/pages/Views/MovieView.tsx
@@ -2,19 +2,35 @@ import { ScrollRestoration, useParams } from "react-router-dom"
 import { getMediaById } from "../../helpers";
 import { Details, VideoSection } from "./components";
 import { useFetch } from "../../hooks/useFetch";
+import { useState } from "react";
 
 
 export const MovieView = () => {
 
   const { movieId = '' } = useParams() as { movieId: string}; 
+  const [ showPopUpVideo , setShowPopUpVideo ] = useState<boolean>( false ); 
   const { data, isLoading } = getMediaById('movie', movieId ); 
   const { data: videoData, isLoading: isLoadingVideos } = useFetch(`https://api.themoviedb.org/3/movie/${movieId}/videos`)
   
   return (
 
     <>
-      <Details data = { data } isLoading={ isLoading } movieId={ movieId } mediaTypeApi='movie' videoData = { videoData }/>
-      <VideoSection videoData = { videoData } isLoadingVideos = { isLoadingVideos }/>
+      <Details 
+        data = { data } 
+        isLoading={ isLoading } 
+        movieId={ movieId } 
+        mediaTypeApi='movie' 
+        videoData = { videoData }
+        showPopUpVideo = { showPopUpVideo }
+        setShowPopUpVideo={ setShowPopUpVideo }
+      />
+      
+      <VideoSection 
+        videoData = { videoData } 
+        isLoadingVideos = { isLoadingVideos }
+        showPopUpVideo = { showPopUpVideo }
+        setShowPopUpVideo={ setShowPopUpVideo }
+      />
       
       <ScrollRestoration/>
     </>
