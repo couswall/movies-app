fb2b6d227b956f91a5d927ae1968c155901bf822
Andre Ignorosa <andreiglu@hotmail.com>
2024-03-19 19:20:16 -0600

    VideoSection.tsx: Props defined

--- movie-app/src/pages/Views/components/VideoSection.tsx
+++ movie-app/src/pages/Views/components/VideoSection.tsx
@@ -1,6 +1,21 @@
+import { MediaVideos } from "../../../interfaces"
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import { Autoplay } from 'swiper/modules';
 
-export const VideoSection = () => {
-  return (
-    <div>VideoSection</div>
+
+interface VideoSectionProps{
+    videoData: MediaVideos, 
+    isLoadingVideos: boolean; 
+}
+
+export const VideoSection: React.FC<VideoSectionProps> = ({videoData, isLoadingVideos}) => {
+  
+    return (
+    <>
+        <section className="container mt-4">
+            <h3>Official Videos</h3>
+        </section>
+    </>
   )
 }
