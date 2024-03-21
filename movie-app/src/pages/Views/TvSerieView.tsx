bb7cc2386bfb23a050a18affa565825f92c989f2
Andre Ignorosa <andreiglu@hotmail.com>
2024-03-20 21:24:23 -0600

    TvSeriesView.tsx defined

--- movie-app/src/pages/Views/TvSerieView.tsx
+++ movie-app/src/pages/Views/TvSerieView.tsx
@@ -1,6 +1,50 @@
+import { useState } from "react";
+import { ScrollRestoration, useParams } from "react-router-dom";
+import { getMediaById } from "../../helpers";
+import { useFetch } from "../../hooks/useFetch";
+import { Details, Recommended, Similar, VideoSection } from "./components";
 
 export const TvSerieView = () => {
+
+  const { tvId = '' } = useParams() as { tvId: string}; 
+  const [ showPopUpVideo , setShowPopUpVideo ] = useState<boolean>( false ); 
+  const { data, isLoading } = getMediaById('tv', tvId ); 
+  const { data: videoData, isLoading: isLoadingVideos } = useFetch(`https://api.themoviedb.org/3/tv/${tvId}/videos`);
+  const { data: similarMedia, isLoading: isLoadingSimilar } = useFetch(`https://api.themoviedb.org/3/tv/${tvId}/similar`);
+  const { data: recommendedMedia, isLoading: isLoadingRecommended} = useFetch(`https://api.themoviedb.org/3/tv/${tvId}/recommendations`);
+
   return (
-    <h1>TvSerieView</h1>
+    <>
+      <Details 
+        data = { data } 
+        isLoading={ isLoading } 
+        movieId={ tvId } 
+        mediaTypeApi='movie' 
+        videoData = { videoData }
+        showPopUpVideo = { showPopUpVideo }
+        setShowPopUpVideo={ setShowPopUpVideo }
+      />
+      
+      <VideoSection 
+        videoData = { videoData } 
+        isLoadingVideos = { isLoadingVideos }
+        showPopUpVideo = { showPopUpVideo }
+        setShowPopUpVideo={ setShowPopUpVideo }
+      />
+
+      <Similar 
+        similarMedia = { similarMedia } 
+        isLoadingSimilar = {isLoadingSimilar}
+        mediaTitle = 'Shows'
+      />
+      
+      <Recommended 
+        recommendedMedia = {recommendedMedia} 
+        isLoadingRecommended={isLoadingRecommended}
+        mediaTitle = 'Shows'
+      />
+
+      <ScrollRestoration />
+    </>
   )
 }
