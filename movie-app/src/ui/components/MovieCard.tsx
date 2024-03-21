e7df51bb1aca7ec17e6c65b0b08a985232874cf6
Andre Ignorosa <andreiglu@hotmail.com>
2024-03-20 19:23:37 -0600

    MovieCard.tsx: Link component changed for useNavigate

--- movie-app/src/ui/components/MovieCard.tsx
+++ movie-app/src/ui/components/MovieCard.tsx
@@ -1,6 +1,7 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { Result } from '../../interfaces';
 import './styles';
+import NoPhoto from '/assets/no-poster.png'; 
 
 interface MediaTypeProps extends Result{
   media_type_props?: string; 
@@ -8,6 +9,7 @@ interface MediaTypeProps extends Result{
 
 export const MovieCard: React.FC<Result & MediaTypeProps > = ({ id,title, name, poster_path, vote_average, release_date, first_air_date, media_type, media_type_props}) => {
 
+  const navigate = useNavigate();
 
   const onFormatDate = ( movieDate: any ) => {
     const date = new Date( movieDate );
@@ -19,12 +21,12 @@ export const MovieCard: React.FC<Result & MediaTypeProps > = ({ id,title, name, 
     return date.toLocaleDateString('en-US', options);
   }
 
+  const posterUrl = poster_path ? 'https://image.tmdb.org/t/p/original/' + poster_path : NoPhoto;  
 
   return (
-    <Link to={ `/${ ( media_type === 'tv' || media_type_props === 'tv' ) ? 'tv-serie' : 'movie'}/${id}`}>
-      <div className="card-movie animate__animated animate__fadeIn">
+      <div className="card-movie animate__animated animate__fadeIn cursor-pointer" onClick={() => navigate(`/${ ( media_type === 'tv' || media_type_props === 'tv' ) ? 'tv-serie' : 'movie'}/${id}`)}>
         <div className="card-img-container position-relative mb-3">
-          <img src={`https://image.tmdb.org/t/p/original/${ poster_path}`} alt={ title || name} loading='lazy'/>  
+          <img src={ posterUrl } alt={ title || name} loading='lazy'/>  
           <div className="raiting-circle position-absolute">
             <span className='fw-bold text-black'>{ (vote_average).toFixed(1) }</span>
           </div>
@@ -35,6 +37,5 @@ export const MovieCard: React.FC<Result & MediaTypeProps > = ({ id,title, name, 
           <p className='text-white-50'>{ onFormatDate( release_date || first_air_date ) }</p>
         </div>
       </div>
-    </Link>
   )
 }
