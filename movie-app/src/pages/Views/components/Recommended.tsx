e201de21baba7e6e60a3085361c8984e9f232f7e
Andre Ignorosa <andreiglu@hotmail.com>
2024-03-20 20:41:01 -0600

    Recommended.tsx defined

--- movie-app/src/pages/Views/components/Recommended.tsx
+++ movie-app/src/pages/Views/components/Recommended.tsx
@@ -1,6 +1,26 @@
+import { Movie } from "../../../interfaces";
+import { Carousel } from "../../../ui/components"
+import { CarouselSkeleton } from "../skeletons"
 
-export const Recommended = () => {
+interface RecommendedProps {
+  recommendedMedia: Movie; 
+  isLoadingRecommended: boolean;
+}
+
+export const Recommended: React.FC<RecommendedProps> = ({recommendedMedia,isLoadingRecommended}) => {
+
+  const { results } = !!recommendedMedia && recommendedMedia; 
+
   return (
-    <div>Recommended</div>
+    <section className="similar-section container mt-4 mb-4">
+        <h3 className="mb-3">Recommended Movies</h3>
+
+        {
+            ( isLoadingRecommended ) 
+                ? <CarouselSkeleton/>  
+                : <Carousel moviesArray={ results }/>
+        }
+
+    </section>
   )
 }
