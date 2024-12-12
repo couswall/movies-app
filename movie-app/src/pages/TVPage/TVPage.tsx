9a4a423e561c2baef5909cb9a1b3b6cfdfca9dd9
Andre Ignorosa <andreiglu@hotmail.com>
2024-12-11 21:41:53 -0600

    feat: implement useTvShows hook in TVPage to display tv shows

--- movie-app/src/pages/TVPage/TVPage.tsx
+++ movie-app/src/pages/TVPage/TVPage.tsx
@@ -1,6 +1,63 @@
+import { useEffect, useState } from "react"
+import { useTvShows } from "./hooks/useTvShows";
+import { ITvShows } from "../../store/tvShows/interfaces/tvShows.interfaces";
+import { Loading, MovieCard } from "../../ui/components";
+import { MediaType } from "../../interfaces";
 
 export const TVPage = () => {
+  const {getTvShows, tvShows, isLoading} = useTvShows();
+  
+  const [nextPage, setNextPage] = useState<number>(1);
+  const [allTvShows, setAllTvShows] = useState<ITvShows[]>([]);
+
+  useEffect(() => {
+    getTvShows(nextPage);
+  }, [nextPage]); 
+
+  useEffect(() => {
+    setAllTvShows(tvShows);
+  }, [tvShows]);
+  
   return (
-    <div>TVPage</div>
+   <>
+    <section className="tv-page-section mt-5">
+      <div className="container">
+        <h2 className="mb-3">{'Explore TV Shows'}</h2>
+
+        <div className="mb-4 d-flex gap-3">
+          <select
+            className="form-select bg-dark text-white w-50"
+            aria-label="Default select example"
+          >
+            <option value="">{'Select Genres'}</option>
+          </select>
+          <select 
+            className="form-select bg-dark text-white w-50" 
+            aria-label="Default select example" 
+            // onChange={handleChangeSorting}
+          >
+            <option defaultValue={""} value={''}>{'Sort by'}</option>
+          </select>
+        </div>
+
+        <div className="row">
+          {tvShows.length > 0 && allTvShows.map(tvShow => (
+            <div className="col-xxl-2 col-lg-3 col-md-3 col-sm-6 col-6 mb-5 animate__animated animate__fadeIn animate__slower" key={ tvShow.id }>
+              <MovieCard
+                id={tvShow.id}
+                title={tvShow.name}
+                name={tvShow.original_name}
+                poster_path={tvShow.poster_path}
+                vote_average={tvShow.vote_average}
+                first_air_date={tvShow.first_air_date}
+                media_type={MediaType.Tv}
+              />
+            </div>
+          ))}
+          {isLoading && <Loading/>}
+        </div>
+      </div>
+    </section>
+   </>
   )
 }
